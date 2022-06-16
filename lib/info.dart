c180278218e76c6bd290351151ed6e636f3dfdc6
MiqueiasCardoso <miiiq.pva@hotmail.com>
2022-06-15 21:46:24 -0400

    Alteracao no Arquivo de fotos

--- lib/info.dart
+++ lib/info.dart
@@ -35,98 +35,98 @@ class ListViewWidget extends StatelessWidget {
             title: 'Claudemir Públio Júnior',
             subtitle: 'Docente EBTT IFMT',
             urlImage:
-                'https://pps.whatsapp.net/v/t61.24694-24/102752586_300455284308633_412660927002318082_n.jpg?ccb=11-4&oh=01_AVyYW8ZTx1lQNewY4OotzrBHzB2pLdv-tQpunxGEKwyiUw&oe=62B5699A',
+            'CLA',
             urLinkedin: 'https://lattes.cnpq.br/6986438491496935'),
         ListTileWidget(
             typeImage: 'LA',
             title: 'Yoji Massago',
             subtitle: 'Docente EBTT IFMT',
             urlImage:
-                'https://pps.whatsapp.net/v/t61.24694-24/287615624_201082858908182_2851988399003640577_n.jpg?ccb=11-4&oh=01_AVxzm6UBV6EI9coWbU0IXrkRgIs9AknSdrfs8c7F3cyd9A&oe=62B51DAD',
+            'YOJ',
             urLinkedin: 'https://lattes.cnpq.br/9137805402174351'),
         ListTileWidget(
             typeImage: 'LI',
             title: 'Michel Ferracini',
             subtitle: 'Discente CSTADS',
             urlImage:
-                'https://media-exp2.licdn.com/dms/image/C5603AQElohzc7acR4w/profile-displayphoto-shrink_200_200/0/1517591134479?e=1660780800&v=beta&t=cZ2PVkn5YjKnp1iFrvdaEt9s5vCxg89GjUgsH7sfnbY',
+            'MIC',
             urLinkedin:
-                'https://www.linkedin.com/in/michel-ferracini-69529463'),
+            'https://www.linkedin.com/in/michel-ferracini-69529463'),
         ListTileWidget(
             typeImage: 'LI',
             title: 'Raul Tavares Cecatto',
             subtitle: 'Discente CSTADS',
             urlImage:
-                'https://media-exp2.licdn.com/dms/image/C4E03AQEd6n-SabnR7Q/profile-displayphoto-shrink_200_200/0/1587613252450?e=1660780800&v=beta&t=19sXoQUIv3bgbX_5LXihC3Py_Mzq5hh95g_UVwJUGtk',
+            'RAU',
             urLinkedin: 'https://www.linkedin.com/in/raulcecatto'),
         ListTileWidget(
             typeImage: 'LI',
             title: 'Arlindo Junior Fernandes Pina',
             subtitle: 'Discente CSTADS',
             urlImage:
-                'https://media-exp2.licdn.com/dms/image/C5603AQEZcTT7hQMrWw/profile-displayphoto-shrink_200_200/0/1653210160441?e=1660780800&v=beta&t=v7l7XIBBrKDKcTJpSP-C7iKu4bcBEnq1w2zjBPazi1M',
+            'LIN',
             urLinkedin:
-                'https://www.linkedin.com/in/arlindo-junior-fernandes-662aa821a'),
+            'https://www.linkedin.com/in/arlindo-junior-fernandes-662aa821a'),
         ListTileWidget(
             typeImage: 'LI',
             title: 'Danilo Batista da Cruz',
             subtitle: 'Discente CSTADS',
             urlImage:
-                'https://media-exp2.licdn.com/dms/image/C4E03AQGLha7V5SU8bQ/profile-displayphoto-shrink_200_200/0/1534733782339?e=1660780800&v=beta&t=lwq3IOzAN5QW6KUiYYhKfuYElxpQJckIPmL4X8QjUGE',
+            'DAN',
             urLinkedin:
-                'https://www.linkedin.com/in/danilo-batista-8679b516a/'),
+            'https://www.linkedin.com/in/danilo-batista-8679b516a/'),
         ListTileWidget(
-            typeImage: 'VA',
+            typeImage: 'LI',
             title: 'João Kleber Alves Neto',
             subtitle: 'Discente CSTADS',
             urlImage:
-                'https://scontent.fbsb9-1.fna.fbcdn.net/v/t1.6435-9/69610652_743930866037080_8748678322721914880_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=174925&_nc_eui2=AeFG-pd_TukvVhBTulHCFArF-SEKyZlyYfn5IQrJmXJh-f9jX5dAzSu_FrPWDM4DboDrD1STSqgfJo5xxMk-1mzb&_nc_ohc=EuIbfT8454UAX9__AmC&_nc_ht=scontent.fbsb9-1.fna&oh=00_AT9N8ZMjcb9kbE4PbMex2xzWB-gLeW94uONR0ZhJtZluMQ&oe=62CBD967',
-            urLinkedin: ''),
+            'JKB',
+            urLinkedin: 'linkedin.com/in/joão-kleber-a-neto-422420242'),
         ListTileWidget(
             typeImage: 'LI',
             title: 'Jucelio da Silva Santos',
             subtitle: 'Discente CSTADS',
             urlImage:
-                'https://media-exp2.licdn.com/dms/image/C5603AQH47oiadJri0A/profile-displayphoto-shrink_200_200/0/1637635403360?e=1660780800&v=beta&t=K0Aqz-9PaFuGvk5vkXkC2Li6aqmLeMnycw7wPM7Vs_g',
+            'JUC',
             urLinkedin: 'https://www.linkedin.com/in/juceliodigital/'),
         ListTileWidget(
-            typeImage: 'VA',
+            typeImage: 'LI',
             title: 'Matheus Augusto dos Santos Santana',
             subtitle: 'Discente CSTADS',
             urlImage:
-                'https://pps.whatsapp.net/v/t61.24694-24/196049341_526085358737394_1358442392608527340_n.jpg?ccb=11-4&oh=cb526136ea542671137b6c876b55a676&oe=62B92FCC',
-            urLinkedin: ''),
+            'MAT',
+            urLinkedin: 'https://www.linkedin.com/in/matheus-augusto-96141b242'),
         ListTileWidget(
             typeImage: 'LI',
             title: 'Miqueias Cardoso Soares',
             subtitle: 'Discente CSTADS',
             urlImage:
-                'https://media-exp2.licdn.com/dms/image/C5603AQF5Hdn8bqjZ5g/profile-displayphoto-shrink_200_200/0/1636423099493?e=1660780800&v=beta&t=WdZ1_3bDfi7foFLS-X1uDzBwqZyvrceuhxc2d8B-gGU',
+            'MIQ',
             urLinkedin:
-                'https://www.linkedin.com/in/miqueias-cardoso-soares-507222224'),
+            'https://www.linkedin.com/in/miqueias-cardoso-soares-507222224'),
         ListTileWidget(
             typeImage: 'LI',
             title: 'Priscila da Silva Batista',
             subtitle: 'Discente CSTADS',
             urlImage:
-                'https://media-exp2.licdn.com/dms/image/C4D03AQFFs6RK9T31MA/profile-displayphoto-shrink_200_200/0/1593608625441?e=1660780800&v=beta&t=5LPInkpA4eOD-a6FVW_lYz2W8GmtiJPyBVsWuSJmIGA',
+            'PRI',
             urLinkedin:
-                'https://www.linkedin.com/in/priscila-batista-972b72186'),
+            'https://www.linkedin.com/in/priscila-batista-972b72186'),
         ListTileWidget(
             typeImage: 'LI',
             title: 'Wagner Daniel Macedo Delmondes',
             subtitle: 'Discente CSTADS',
             urlImage:
-                'https://scontent.fbsb9-1.fna.fbcdn.net/v/t1.6435-9/50192530_966011323597379_3190612182064693248_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeEx0k2tbcXz1WDOnCixqWpxlQL8R2ZgUMOVAvxHZmBQw47jMbeiZdIQjLsHz3-Ow0TnDzZIXaSftw1Em8hUPUDq&_nc_ohc=4QL6ZvjWz5oAX_i2AC_&_nc_ht=scontent.fbsb9-1.fna&oh=00_AT8gtYciTDqEnUFtsE_ANoP0-gancvI8ZpBxsemYprK88w&oe=62CC2C11',
-            urLinkedin: ''),
+            'WGN',
+            urLinkedin: 'https://www.linkedin.com/in/wagnerdelmondes'),
         ListTileWidget(
             typeImage: 'LI',
             title: 'Wanderson Renato Moraes da Silva',
             subtitle: 'Discente CSTADS',
             urlImage:
-                'https://media-exp2.licdn.com/dms/image/C4D03AQEZt2H3KzUoGA/profile-displayphoto-shrink_200_200/0/1651629375414?e=1660780800&v=beta&t=qxvmJhnV3Y-qDKTnBIuzCI1NWYmERGFnB_auY5Rx8ds',
+            'WAN',
             urLinkedin:
-                'https://www.linkedin.com/in/wanderson-renato-moraes-da-silva-b49681239/'),
+            'https://www.linkedin.com/in/wanderson-renato-moraes-da-silva-b49681239/'),
       ]).toList(),
     );
   }
@@ -140,19 +140,19 @@ class ListTileWidget extends StatelessWidget {
   final String typeImage;
   const ListTileWidget(
       {key,
-      required this.title,
-      required this.subtitle,
-      required this.urlImage,
-      required this.urLinkedin,
-      required this.typeImage})
+        required this.title,
+        required this.subtitle,
+        required this.urlImage,
+        required this.urLinkedin,
+        required this.typeImage})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      leading: CircleAvatar(
+      leading:  CircleAvatar(
         radius: 25,
-        backgroundImage: NetworkImage(urlImage),
+        backgroundImage: AssetImage("img/" + urlImage + ".png"),
       ),
       title: Text(
         title,
